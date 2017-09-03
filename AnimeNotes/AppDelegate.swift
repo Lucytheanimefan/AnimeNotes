8c91ca53a856da2f8842a63971de66e75dd64bea
Lucy Zhang <lucyzhang@lucytheanimefan-2.local>
2017-09-03 16:56:41 -0400

    Initial Commit

---
+++ AnimeNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AnimeNotes
+//
+//  Created by Lucy Zhang on 9/3/17.
+//  Copyright © 2017 Lucy Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
