ff37234b541cc4f0f2e1318bf29415ed00d9a2f7
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-09-03 17:40:55 -0400

    Added files, started project, added outline view for notes per episode

--- AnimeNotes/ViewController.swift
+++ AnimeNotes/ViewController.swift
@@ -10,6 +10,13 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var outlineView: NSOutlineView!
+    
+    lazy var malAnimeEntries:[NSDictionary] = {
+        print(CFPreferencesCopyValue("malEntries" as CFString, "com.lucy.anime" as CFString, kCFPreferencesCurrentUser, kCFPreferencesAnyHost))
+        return CFPreferencesCopyValue("malEntries" as CFString, "com.lucy.anime" as CFString, kCFPreferencesCurrentUser, kCFPreferencesAnyHost) as! [NSDictionary]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,3 +32,50 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSOutlineViewDelegate
+{
+    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+        var cellView: NSTableCellView!
+        if (tableColumn?.identifier == "AnimeEntryColumn")
+        {
+            cellView = outlineView.make(withIdentifier: "AnimeEntry", owner: nil) as! NSTableCellView
+            if let entry = item as? NSDictionary{
+                cellView.textField?.stringValue = entry["title"] as! String
+            }
+            else if let entry = item as? String
+            {
+                cellView.textField?.stringValue = entry
+            }
+            
+        }
+        return cellView
+    }
+}
+
+extension ViewController: NSOutlineViewDataSource
+{
+    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+        print(item.debugDescription)
+        if let entry = item as? NSDictionary{
+            return entry["total_episodes"] as! NSInteger
+        }
+        else{
+            return self.malAnimeEntries.count
+        }
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        if (item as? NSDictionary) != nil
+        {
+            return String(index)
+        }
+        else
+        {
+            return self.malAnimeEntries[index]
+        }
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+        return ((item as? NSDictionary) != nil)
+    }
+}
